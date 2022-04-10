aa6300fbe475b204005515329ab52a42b01f9be6
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-04-10 20:22:03 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    public int num1 = 0;
+    void Start()
+
+    {
+        num1 = num1 + 2;
+        Debug.Log(num1);
+
+        num1 = num1 * num1;
+        Debug.Log(num1);
+    }
+    
+    
+}
